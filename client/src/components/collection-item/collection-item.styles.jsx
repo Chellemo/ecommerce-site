5815d836ceee6f16b37711434fe55778fa901232
Michelle Smith <chellemo23@gmail.com>
2020-12-30 00:25:34 -0500

    added optimizations for mobile responsiveness and performance - memoized CartItem and added React lazy, Suspense and added ErrorBoundary

--- client/src/components/collection-item/collection-item.styles.jsx
+++ client/src/components/collection-item/collection-item.styles.jsx
@@ -12,16 +12,26 @@ export const CollectionItemContainer = styled.div`
 
   &:hover {
     .image {
-      opacity: unset;
+      opacity: 0.8;
     }
-
     button {
-      opacity: unset;
+      opacity: 0.85;
+      display: flex;
     }
   }
 
   @media screen and (max-width: 800px) {
     width: 40vw;
+
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+  
+      button {
+        opacity: unset;
+      }
+    }
 }
 `;
 
